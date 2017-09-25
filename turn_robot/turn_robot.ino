9f80d8bb9a7b984c1431911a5548bb620f19a637
Adam Jarzebak <adam@jarzebak.eu>
2017-09-25 20:05:44 +0100

    Make robot turn however not finish yet

--- turn_robot/turn_robot.ino
+++ turn_robot/turn_robot.ino
@@ -18,6 +18,7 @@
 #define READIVS             0x2C                    // Byte reads motor currents and battery voltage        
 #define READENCS            0x25                    // Byte reads both encoders
 #define GET_VER             0x29
+#define TURN                0x32                    // Byte writes speed to motor 2 <<<------------------------- This was added.
 
 #define LCD_RX              0x02                    // RX and TX pins used for LCD0303 serial port
 #define LCD_TX              0x03
@@ -52,31 +53,47 @@ void setup(){
 }
 
 void loop(){ 
-  while(encValue < 600){               // While encoder 1 value less than 3000 move forward
+  while(encValue < 180){               // While encoder 1 value less than 3000 move forward
+    
     Serial.write(CMD);                  // Set motors to drive forward at full speed
     Serial.write(WRITESP1);
-    Serial.write(154);
+    Serial.write(200);
     encValue = readEncoder();
+    
   }
   
-  delay(3 * s);
+  delay(1 * s);
   
   while(encValue > 0){
     
     Serial.write(CMD);                  // Drive motors reverse at full speed
     Serial.write(WRITESP1);
-    Serial.write(100);
+    Serial.write(10);
     encValue = readEncoder();
 
   }
+
+  delay(1 * s);
+    
+//  Turn robot
+
+  Serial.write(CMD); // Set motors to drive forward at full speed
+  Serial.write(WRITESP1); 
+  Serial.write(1);
+  Serial.write(CMD);  //<---- This was missing
+  Serial.write(TURN); 
+  Serial.write(40);
   
-  
+
+    
  delay(3 * s);
  
  }
 
 
-long readEncoder(){                        // Function to read and display the value of both encoders, returns value of first encoder
+ 
+
+ long readEncoder(){                        // Function to read and display the value of both encoders, returns value of first encoder
   long result1 = 0; 
   long result2 = 0;
   Serial.write(CMD);
